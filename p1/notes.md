ae50e16a8ad7a2a5683357a8986625ee63f4c68c
ssik64@gmail.com <ssik64@gmail.com>
2016-10-20 13:03:00 +0200

    rearanging git

---
+++ p1/notes.md
@@ -0,0 +1,9 @@
+#test
+htacces
+html
+css
+apache
+status kodovi sa servera
+phpinfo
+mjesanje html-a i phpa (: endif)
+cisti php ne ?>
---
+++ p1/page2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Hello world</h1>
+        <p>ishdgoshjakljgklsdfgjaskdgnsfklgnakldgsdhngkladnhgklsdgnklasdgnkladnhgkl</p>
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSQaH0dYVr_fZFc9otcg8K0zkG2c8tsYT_8z4v5U2WKvuJPudGs">
+        <?php if(date('d.m.Y.')=='20.10.2016.'): ?>
+        <p>welcome</p>
+            <?php endif ?>
+        <footer><p>date : <?php echo date('d.m.Y.')?></p></footer>
+    </body>
+</html>
